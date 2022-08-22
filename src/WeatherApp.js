00a345dbba2c0fcae022ee03a39602dbd74e4d63
yasminlawre <yasminlawre111@gmail.com>
2022-08-22 09:45:09 +0100

    added bootstrap

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -2,15 +2,18 @@ import Form from "./Form";
 import Headings from "./Headings";
 import Description from "./Description";
 import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.css";
 import "./weather.css";
 
 export default function App() {
   return (
-    <div className="App container">
-      <Form />
-      <Headings />
-      <Description />
-      <Footer />
+    <div className="App">
+      <div className="container">
+        <Form />
+        <Headings />
+        <Description />
+        <Footer />
+      </div>
     </div>
   );
 }
