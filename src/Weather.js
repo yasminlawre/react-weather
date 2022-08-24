0847e018d1ce29e52e9d6d3acc673008f3fbd08c
yasminlawre <yasminlawre111@gmail.com>
2022-08-24 13:16:39 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ export default function Weather(props) {
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: new date(response.data.dt * 1000),
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
