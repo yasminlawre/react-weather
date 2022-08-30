eb145ac2348bb6a8a47dc040f77bbe252cf2e732
yasminlawre <yasminlawre111@gmail.com>
2022-08-30 08:47:07 +0100

    weather forecast layout

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,7 +15,7 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6 details">
-          <WeatherIcon code={props.data.icon} />
+          <WeatherIcon code={props.data.icon} size={52} />
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
 
